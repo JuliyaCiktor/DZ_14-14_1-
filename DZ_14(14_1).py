62bedf17fb01401faed4792fc3bc3b93e83d1b16
JuliyaCiktor <julia_uk_oleksandrivna@ukr.net>
2024-10-04 10:05:57 +0300

    commit

---
+++ DZ_14(14_1).py
@@ -0,0 +1,62 @@
+class GroupOverflowError(Exception):
+    def __init__(self, message="Група переповнена. Неможливо додати більше 10 студентів."):
+        self.message = message
+        super().__init__(self.message)
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age} years old, {self.gender}'
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age} years old, {self.gender}, Record Book: {self.record_book}'
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupOverflowError()
+        self.group.add(student)
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+    def __str__(self):
+        all_students = "\n".join([str(student) for student in self.group])
+        return f'Group Number: {self.number}\nStudents:\n{all_students}'
+
+
+# Тестування
+try:
+    st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+    st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+
+    gr = Group('PD1')
+
+    # Додаємо 10 студентів
+    for i in range(10):
+        gr.add_student(Student('Male', 20 + i, f'Student{i}', f'LastName{i}', f'RB{i}'))
+
+    # Спроба додати 11-го студента
+    gr.add_student(st1)  # Тут буде піднято виняток GroupOverflowError
+
+except GroupOverflowError as e:
+    print(e)  # Виведення повідомлення про переповнення
+
+print(gr)
+
+# Перевірка пошуку студента
+assert str(gr.find_student('LastName0')) == str(Student('Male', 20, 'Student0', 'LastName0', 'RB0')), 'Test1'
+assert gr.find_student('NonExisting') is None, 'Test2'
